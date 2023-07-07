9f9be1685493822243c0a485c00aa67c715373c1
zulyta <luzta48@gmail.com>
2023-07-07 17:15:34 -0500

    feat: :closed_lock_with_key: Connect Fronted

--- fronted/src/configContracts.js
+++ fronted/src/configContracts.js
@@ -4,13 +4,21 @@ import { Contract, JsonRpcProvider, ethers } from "ethers";
 
 window.ethers = ethers;
 
-function initSCsMumbai() {
+async function initSCsMumbai() {
   const urlProvider =
     "https://polygon-mumbai.g.alchemy.com/v2/ImL76Y58Mvk3w8TxYK843RPMK7dNJ8RQ ";
   const provider = new JsonRpcProvider(urlProvider);
+  
+  const nftAddress = "0xAb68c60C5d74720c6456319F9132E6f47d22D7d3";
+  const rentcAddress = "0xcdCC6a394d8f30DC43Be1Bc9aE9f42D98d2c8d13";
 
-  const nftAddress = "0x42F5E1D2cA39eCD348001a9F28ac03D06ffFE9f4";
-  const rentcAddress = "0xCDAEb5445Fc45E5548097a18Dc3640A2D84Ef0A8";
+  const number = await provider.getBlockNumber();
+  
+
+  // Look up the current block number (i.e. height)
+  console.log('number',number)
+
+  const signer = await provider.getSigner();
 
   const nftTknContract = new Contract(nftAddress, nftTknAbi.abi, provider);
   const rentCarContract = new Contract(
@@ -18,6 +26,36 @@ function initSCsMumbai() {
     rentCarTknAbi.abi,
     provider
   );
+
+  // Get the current balance of an account (by address or ENS name)
+  //const signer = provider.getSigner();
+  //const addr = await nftTknContract.connect(signer).getAddress()
+  //const balance = await provider.getBalance(addr)
+  //console.log('number',balance)
+
+  // The symbol name for the token
+  const sym = await nftTknContract.symbol()
+  console.log('sym',sym)
+
+  const to = '0x860cb92096D13b34E6d5638f68d6F1B6be77CfC9'
+  const uri = 'https://rentcarnft.infura-ipfs.io/ipfs/QmYXstdC7ZKdoZVW6yy4Yifo8Pa9ogTSGs9zrD4CKU6bex'
+  const nameAuto = 'Tico'
+  const features = 'Caracteristicas'
+  const price = 1523
+  const guarantee = 1523
+  const interestRate = 50
+
+  const nft = await nftTknContract.safeMintOwner(to,uri,nameAuto,features,price,guarantee,interestRate)
+
+  console.log('nft',nft)
+
+  //const sym2 = await rentCarContract.symbol()
+  //console.log('sym2',sym2)
+
+  //const balance = await nftTknContract.balanceOf("ethers.eth")
+  //console.log('balance',balance)
+
+  
 
   return { nftTknContract, rentCarContract };
 }
