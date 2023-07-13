7360dc2e282674b909ea54f3002f16c23c806e85
rolandomarzano <rolo.marzano@gmail.com>
2023-07-12 23:44:44 -0500

    NuxtlabsUI modal added in index view

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -8,15 +8,29 @@
         <div class="nft-footer">
           <h3>{{ nft.nameAuto }}</h3>
           <h4># {{ nft.tokenId }}</h4>
-          <button class="btn-lend-nft">Alquilar NFT</button>
+          <UButton
+            label="Alquilar NFT"
+            color="emerald"
+            size="lg"
+            block
+            @click="isOpen = true"
+          />
         </div>
       </div>
     </div>
+    <UModal v-model="isOpen">CONTENIDO NFT</UModal>
   </div>
 </template>
 <script setup>
 const { getNFTs, nftList } = useEthers();
+
 onMounted(() => {
   getNFTs();
 });
+
+onUnmounted(() => {
+  nftList.value = [];
+});
+
+const isOpen = ref(false);
 </script>
